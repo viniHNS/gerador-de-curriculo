79388a06ec2d1f9e6b6902b44cc321d6262771a8
Vinicius Nishida <viniciusnishida13@gmail.com>
2023-05-24 15:49:50 -0300

    adicionado conteudo para a pagina inicial

--- php/index.php
+++ php/index.php
@@ -1,6 +1,9 @@
 
 <?php require "navbar.php"?>
 
+    <div class="content-homepage">
+        <p class="paragraph-homepage">Utilize a barra superior para navegar pelo site</p>
+    </div>
     
 
 <?php require "footer.php"?>
